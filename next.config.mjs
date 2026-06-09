de075bd7387f0053e1985718bd78185e0f9845bd
luiz <luizsantost4@gmail.com>
2026-06-08 23:33:34 -0300

    chore: configura exportacao estatica e workflow do github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubActions ? '/spam-project' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
